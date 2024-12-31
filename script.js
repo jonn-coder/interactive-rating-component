304f5bb1b9dabe60a882abe42fbbbda48affb824
jonn-coder <johnonah2525@gmail.com>
2024-12-31 15:47:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
+// element vars
 let rating = document.querySelector(".rating-container")
 let thankYou = document.querySelector(".thank-you-container")
 let ratingNums = document.querySelectorAll(".rating-num li")
 let submitBtn = document.querySelector(".submit")
+let selectedRate = document.querySelector(".rating-selected")
 
-let rateNum = Number()
+// assign id to all rate to be selected
+ratingNums.forEach((num, index) => {
+  num.setAttribute("id", index)
+  num.setAttribute("class", "rateNum")
+})
+
+//other vars 
+let rateNum = 0;
 let selected = false
+// select each rate on clicking
+addEventListener
 document.addEventListener("click", (e) => {
   let target = e.target
-  rateNum = Number(target.textContent)
-  if (isNaN(rateNum)) {
-    if (!target.classList.contains('submit')) {
-    ratingNums.forEach((num) => {
-      num.style.backgroundColor = ""
-      num.style.color = ""
-    })
+//   return if no rate was selected
+  if (!target.classList.contains("rateNum")) {
     return
-}
-    else {
-        return
-    }
   }
 
-  rateNum = target.textContent
-  selected = true;
+//  rateNum should be set to number so as to be used on accesing other rating 
+  rateNum = Number(target.id)
+  selected = true
+
+//  set white backgroung on focus/selected rating
   ratingNums.forEach((num) => {
     num.style.backgroundColor = ""
     num.style.color = ""
   })
-  ratingNums[rateNum-1].style.backgroundColor = "hsl(0, 0%, 100%)"
-  ratingNums[rateNum-1].style.color = "hsl(213, 19%, 18%)"
+
+  ratingNums[rateNum].style.backgroundColor = "hsl(0, 0%, 100%)"
+  ratingNums[rateNum].style.color = "hsl(213, 19%, 18%)"
 })
 
+// listing for click event on submit button
 submitBtn.addEventListener("click", () => {
+  // return if no rating was selected
   if (!selected) return
   rating.classList.add("fade-out")
+//thank you state to be display after rating state animated completed(fade out)
   rating.addEventListener("animationend", () => {
     rating.style.display = "none"
     thankYou.style.display = "block"
+    selectedRate.innerHTML = `You selected ${rateNum + 1} out of ${ratingNums.length}`
     thankYou.classList.add("fade-in")
     thankYou.addEventListener("animationend", () => {
       thankYou.style.display = "block"
